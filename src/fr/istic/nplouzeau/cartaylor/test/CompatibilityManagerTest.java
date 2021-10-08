ef19676a4b3841f788e29f22ee9a67680bb9067f
Delourmel Arnaud <arnaud.delourmel@etudiant.univ-rennes1.fr>
2021-10-08 17:29:29 +0200

    Add class test CompatibilityManagerTest

---
+++ src/fr/istic/nplouzeau/cartaylor/test/CompatibilityManagerTest.java
@@ -0,0 +1,24 @@
+package fr.istic.nplouzeau.cartaylor.test;
+
+public class CompatibilityManagerTest {
+    /*
+     * Test pour la méthode addIncompatibilities
+     * Test addIncopatibilities classique
+     * Test avec avec un élément qui est déjà dans la liste des incompatibilitées
+     * Test avec avec un élément qui est dans la liste des requirements
+     *
+     * Test pour la méthode removeIncompatibility
+     * Test classique
+     * Test avec un élément pas présent dans la liste
+     *
+     *
+     * Test pour la méthode addRequirements
+     * Test addIncopatibilities classique
+     * Test avec avec un élément qui est déjà dans la liste des requirements
+     * Test avec avec un élément qui est dans la liste des imcompatibilitées
+     *
+     * Test pour la méthode removeRequirement
+     * Test classique
+     * Test avec un élément pas présent dans la liste
+     */
+}
