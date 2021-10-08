3a6ebb2c15109283e891fe94dc4051bf8270ceba
Delourmel Arnaud <arnaud.delourmel@etudiant.univ-rennes1.fr>
2021-10-08 16:46:12 +0200

    Beginning javadoc

--- src/fr/istic/nplouzeau/cartaylor/api/CompatibilityChecker.java
+++ src/fr/istic/nplouzeau/cartaylor/api/CompatibilityChecker.java
@@ -3,9 +3,18 @@ package fr.istic.nplouzeau.cartaylor.api;
 import java.util.Set;
 
 public interface CompatibilityChecker {
-
+    /**
+     * Allow to get the incompatible elements of the PartType
+     * @param reference reference of the element
+     * @return The incompatible elements of the PartType
+     */
     Set<PartType> getIncompatibilities(PartType reference);
 
+    /**
+     * Allow the get the requirements for the reference
+     * @param reference
+     * @return The requirements of the PartType
+     */
     Set<PartType> getRequirements(PartType reference);
 
 }
--- src/fr/istic/nplouzeau/cartaylor/api/CompatibilityManager.java
+++ src/fr/istic/nplouzeau/cartaylor/api/CompatibilityManager.java
@@ -3,13 +3,32 @@ package fr.istic.nplouzeau.cartaylor.api;
 import java.util.Set;
 
 public interface CompatibilityManager extends CompatibilityChecker {
-
+    /**
+     * Allow to add new incompatibilities for a PartType
+     * @param reference The reference of the PartType to modified
+     * @param target Collection of the new incompatibilities
+     */
     void addIncompatibilities(PartType reference, Set<PartType> target);
 
+    /**
+     * Allow to remove one incompatibilie of one PartType
+     * @param reference
+     * @param target The incompatibilitie to remove
+     */
     void removeIncompatibility(PartType reference, PartType target);
 
+    /**
+     * Allow to add new requirements of one PartType
+     * @param reference
+     * @param target The new requirements to add
+     */
     void addRequirements(PartType reference, Set<PartType> target);
 
+    /**
+     * Allow to remove one requirement of one PartType
+     * @param reference
+     * @param target The requirement to remove
+     */
     void removeRequirement(PartType reference, PartType target);
 
 }
