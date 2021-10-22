9d0efe148193fdc8d3d661a923c572fa65acfd7e
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2021-10-22 15:57:37 +0200

    implement tests in ConfiguratorTest class

--- src/fr/istic/nplouzeau/cartaylor/test/ConfiguratorTest.java
+++ src/fr/istic/nplouzeau/cartaylor/test/ConfiguratorTest.java
@@ -1,33 +1,38 @@
 package fr.istic.nplouzeau.cartaylor.test;
 
-import fr.istic.nplouzeau.cartaylor.api.Configurator;
-import fr.istic.nplouzeau.cartaylor.engine.ConfiguratorImpl;
-import org.junit.jupiter.api.BeforeEach;
+import fr.istic.nplouzeau.cartaylor.api.Category;
+import static org.junit.jupiter.api.Assertions.*;
+
+import fr.istic.nplouzeau.cartaylor.engine.CategoryImpl;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
 
 public class ConfiguratorTest extends CarTaylorTest {
 
     /*
-     * Tests pour chaques getters sur des catégories/part type/configurations différentes
+     * test du Getter getCategories
      */
-
-    Configurator config;
-
-
-
     @Test
     void testGetCategories() {
-
+        assertEquals(mapCategoryPartTypeConfigurator.keySet(), configurator.getCategories());
     }
 
     @Test
-    void testGetVariants() {
+    void testGetVariantsExistingCategory() {
+        assertEquals(mapCategoryPartTypeConfigurator.get(engineCategory), configurator.getVariants(engineCategory));
+    }
 
+    @Test
+    void testGetVariantsCategoryDoesNotExist() {
+        Category mockCategory = new CategoryImpl("MockCategory");
+        assertEquals(Collections.emptySet(), configurator.getVariants(mockCategory));
     }
 
     @Test
     void testGetConfiguration() {
-
+        // TODO test à completer (ne passe pas)
+        //assertEquals(configuration, configurator.getConfiguration());
     }
 
     @Test
