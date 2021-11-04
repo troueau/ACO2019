09740890f8ee9eb8f0f6baeb815d1a4e67648ee4
trousseau <tom.rousseau@etudiant.univ-rennes1.fr>
2021-11-04 10:39:46 +0100

    add comments on tests and remove useless lines

--- src/fr/istic/nplouzeau/cartaylor/test/CompatibilityManagerTest.java
+++ src/fr/istic/nplouzeau/cartaylor/test/CompatibilityManagerTest.java
@@ -21,12 +21,17 @@ public class CompatibilityManagerTest extends CarTaylorTest {
 
     }
 
-
+    /*
+     * test du constructeur de CompatibilityManager
+     */
     @Test
     void testConstructor() {
         assertDoesNotThrow(() -> new CompatibilityManagerImpl(null, null));
     }
 
+    /*
+     * test d'ajout d'incompatibilities, aucune existantes precedemment
+     */
     @Test
     void testAddIncompatibilitiesWhenNoPreviousIncompatibilities() {
         Set<PartType> incompatibilitiesEG100 = Set.of(tm5, ta5);
@@ -40,6 +45,9 @@ public class CompatibilityManagerTest extends CarTaylorTest {
         assertEquals(incompatibilitiesEG100, compatibilityManager.getIncompatibilities(eg100));
     }
 
+    /*
+     * test d'ajout d'incompatibilities, d'autres présentes precedemment
+     */
     @Test
     void testAddIncompatibilitiesWhenThereIsPreviousIncompatibilities() {
         Set<PartType> incompatibilities2Tsf7 = Set.of(xc, in);
@@ -55,13 +63,19 @@ public class CompatibilityManagerTest extends CarTaylorTest {
         }
 
         assertEquals(expected, compatibilityManager.getIncompatibilities(tsf7));
-
     }
+
+    /*
+     * test d'ajout d'incompatibilities, une exception est levée
+     */
     @Test
     void testAddIncompatibilitiesException() {
         assertThrows(AlreadyManageException.class, () -> compatibilityManager.addIncompatibilities(tc120, requirementsTC120));
     }
 
+    /*
+     * test d'ajout de requirements, aucune presentes precedemment
+     */
     @Test
     void testAddRequirementsWhenNoPreviousRequirements() {
         Set<PartType> requirementsEG100 = Set.of(tm5, ta5);
@@ -73,8 +87,11 @@ public class CompatibilityManagerTest extends CarTaylorTest {
         }
 
         assertEquals(requirementsEG100, compatibilityManager.getRequirements(eg100));
-
     }
+
+    /*
+     * test d'ajout de requirements, d'autres presentes precedemment
+     */
     @Test
     void testAddRequirementsWhenThereIsPreviousRequirements() {
         Set<PartType> requirements2Tc120 = Set.of(xc, in);
@@ -90,13 +107,19 @@ public class CompatibilityManagerTest extends CarTaylorTest {
         }
 
         assertEquals(expected, compatibilityManager.getRequirements(tc120));
-
     }
+
+    /*
+     * test d'ajout de requirements, une exception est levée
+     */
     @Test
     void testAddRequirementsException() {
         assertThrows(AlreadyManageException.class, () -> compatibilityManager.addRequirements(tsf7, incompatibilitiesTSF7));
     }
 
+    /*
+     * test de l'ajout de requirements symmétrique, c-a-d si on ajoute des requirements pour une PartType, ajoute aussi aux PartType required la PartType source
+     */
     @Test
     void testAddRequirementsSymetric() {
         Set<PartType> partTypeToAdd = new HashSet<>();
@@ -114,25 +137,43 @@ public class CompatibilityManagerTest extends CarTaylorTest {
         assertEquals(expectedSetOfPartTypeRequiredForXM, compatibilityManager.getRequirements(xm));
     }
 
+    /*
+     * test remove incompatibilites, une exception est levée
+     */
     @Test
     void testRemoveIncompatibilityWhenRefNotExist() {
         assertDoesNotThrow(() -> compatibilityManager.removeIncompatibility(null, eg100));
     }
+
+    /*
+     * test remove incompatibilites, une exception est levée
+     */
     @Test
     void testRemoveIncompatibilityWhenTargetNotExist() {
         assertDoesNotThrow(() -> compatibilityManager.removeIncompatibility(tsf7, null));
     }
 
+    /*
+     * test remove incompatibilites, on verifie qu'aucune exception est levée
+     */
     @Test
     void testRemoveIncompatibilityWhenTargetIsNotInIncompatibilities() {
         assertDoesNotThrow(() -> compatibilityManager.removeIncompatibility(tsf7, is));
         assertEquals(Set.of(eg100, eg133, ed110), compatibilityManager.getIncompatibilities(tsf7));
     }
+
+    /*
+     * test remove incompatibilites, on verifie qu'aucune exception est levée
+     */
     @Test
     void testRemoveIncompatibilityWhenMappingValueIsNull() {
         assertDoesNotThrow(() -> compatibilityManager.removeIncompatibility(xs, is));
         assertEquals(Collections.emptySet(), compatibilityManager.getIncompatibilities(xs));
     }
+
+    /*
+     * test remove incompatibilites, on verifie qu'aucune exception est levée
+     */
     @Test
     void testRemoveIncompatibility() {
         Set<PartType> expectedIncompatibilitiesList = Set.of(eg133, ed110);
@@ -140,59 +181,47 @@ public class CompatibilityManagerTest extends CarTaylorTest {
         assertEquals(expectedIncompatibilitiesList, compatibilityManager.getIncompatibilities(tsf7));
     }
 
+    /*
+     * test remove requirements, on verifie qu'aucune exception est levée
+     */
     @Test
     void testRemoveRequirementWhenRefNotExist() {
         assertDoesNotThrow(() -> compatibilityManager.removeRequirement(null, eg100));
     }
+
+    /*
+     * test remove requirements, on verifie qu'aucune exception est levée
+     */
     @Test
     void testRequirementsWhenTargetNotExist() {
         assertDoesNotThrow(() -> compatibilityManager.removeRequirement(eh120, null));
     }
 
+    /*
+     * test remove requirements, on verifie qu'aucune exception est levée
+     */
     @Test
     void testRemoveRequirementWhenTargetIsNotInRequirements() {
         assertDoesNotThrow(() -> compatibilityManager.removeRequirement(tc120, is));
         assertEquals(Set.of(eh120), compatibilityManager.getRequirements(tc120));
     }
+
+    /*
+     * test remove requirements, on verifie qu'aucune exception est levée
+     */
     @Test
     void testRemoveRequirementyWhenMappingValueIsNull() {
         assertDoesNotThrow(() -> compatibilityManager.removeRequirement(xs, is));
         assertEquals(Collections.emptySet(), compatibilityManager.getRequirements(xs));
     }
 
+    /*
+     * test remove requirements, on verifie qu'aucune exception est levée
+     */
     @Test
     void testRemoveRequirement() {
         assertDoesNotThrow(() -> compatibilityManager.removeRequirement(tc120, eh120));
         assertEquals(Collections.emptySet(), compatibilityManager.getRequirements(tc120));
     }
-//
-//    @Test
-//    void testAddIncompatibiliitesException() {
-//
-//    }
 
-
-
-
-
-    /*
-     * Test pour la méthode addRequirements
-     * Test addIncopatibilities classique
-     * Test avec avec un élément qui est déjà dans la liste des incompatibilitées
-     * Test avec avec un élément qui est dans la liste des requirements
-     *
-     * Test pour la méthode removeIncompatibility
-     * Test classique
-     * Test avec un élément pas présent dans la liste
-     *
-     *
-     * Test pour la méthode addRequirements
-     * Test addIncopatibilities classique
-     * Test avec avec un élément qui est déjà dans la liste des requirements
-     * Test avec avec un élément qui est dans la liste des imcompatibilitées
-     *
-     * Test pour la méthode removeRequirement
-     * Test classique
-     * Test avec un élément pas présent dans la liste
-     */
 }
