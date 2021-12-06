3bd5dd7acfb550f947dd02e483a525ac5a3d6dc8
Delourmel Arnaud <arnaud.delourmel@etudiant.univ-rennes1.fr>
2021-12-06 16:31:29 +0100

    Complete PropertyManagerImpl

--- src/fr/istic/nplouzeau/cartaylor/engine/PropertyManagerImpl.java
+++ src/fr/istic/nplouzeau/cartaylor/engine/PropertyManagerImpl.java
@@ -2,34 +2,68 @@ package fr.istic.nplouzeau.cartaylor.engine;
 
 import fr.istic.nplouzeau.cartaylor.api.PropertyManager;
 
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * Implementation of the interface PropertyManager
  * @author Arnaud DELOURMEL, Tom ROUSSEAU
  */
 public class PropertyManagerImpl implements PropertyManager {
-    @Override
-    public Set<String> getPropertyNames() {
-        // TODO
-        return null;
+
+    private class Property {
+        public final Supplier<String> getter;
+        public final Consumer<String> setter;
+        public final Set<String> possibleValues;
+
+        Property(Supplier<String> getter, Consumer<String> setter, Set<String> possibleValues) {
+            this.getter = getter;
+            this.setter = setter;
+            this.possibleValues = possibleValues;
+
+        }
     }
 
+    private final Map<String, Property> properties = new HashMap<>();
+
+
+    protected void addProperty(String name, Supplier<String> getter, Consumer<String> setter,
+                               Set<String> possibleValues) {
+        properties.put(name, new Property(getter, setter, possibleValues));
+    }
     @Override
-    public Set<String> getAvailablePropertyValues(String propertyName) {
-        // TODO
-        return null;
+    public Set<String> getPropertyNames() {
+        return Collections.unmodifiableSet(properties.keySet());
     }
 
     @Override
     public Optional<String> getProperty(String propertyName) {
-        //TODO
+        Objects.requireNonNull(propertyName);
+
+        if (properties.containsKey(propertyName)) {
+            return Optional.of(properties.get(propertyName).getter.get());
+        }
         return Optional.empty();
     }
 
     @Override
     public void setProperty(String propertyName, String propertyValue) {
-        //TODO
+        Objects.requireNonNull(propertyName);
+        Objects.requireNonNull(propertyValue);
+
+        if ((properties.containsKey(propertyName)) && (properties.get(propertyName).setter != null)) {
+            properties.get(propertyName).setter.accept(propertyValue);
+        } else {
+            throw new IllegalArgumentException("bad property name or value: " + propertyName);
+        }
+    }
+
+    @Override
+    public Set<String> getAvailablePropertyValues(String propertyName) {
+        if (properties.containsKey(propertyName)) {
+            return Collections.unmodifiableSet(properties.get(propertyName).possibleValues);
+        }
+        return Collections.emptySet();
     }
 }
